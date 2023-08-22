719d10063f1a83340733da9b54b0870c74685d61
TristanBester <tristanbester@gmail.com>
2023-08-22 11:28:20 +0200

    Updated mappings

--- mappings.lua
+++ mappings.lua
@@ -5,15 +5,26 @@ M.abc = {
     ["<leader>e"] = {":NvimTreeToggle <CR>", "Toggle NvimTree"},
     ["<leader>w"] = {":NvimTreeFocus <CR>", "Focus NvimTree"},
     ["<leader>vs"] = {"<cmd>:VenvSelect<cr>", "Select virtual env"},
-		["<leader>vc"] = {"<cmd>:VenvSelectCached<cr>", "Select cached virtual env"}
+		["<leader>vc"] = {"<cmd>:VenvSelectCached<cr>", "Select cached virtual env"},
+    ["<C-l>"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "Toggle floating term",
+    },
+    ["<C-h>"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "Toggle horizontal term",
+    },
   },
-
   i = {
      ["jj"] = { "<ESC>", "escape insert mode" , opts = { nowait = true }},
   }
 }
 
-return M 
+return M
 
 
 
